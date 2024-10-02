ef1c1c9c90083bd775b8f908044e6b8e17bb6aa5
Parva <parvadesai404@gmail.com>
2024-10-02 22:25:59 +0530

    [Add] grass header file

---
+++ Header/Grass.hpp
@@ -0,0 +1,15 @@
+﻿#include<string>
+#include<vector>
+
+struct Grass
+{
+    std::string environmentType;
+    std::vector<WildPokemon> wildPokemonList;
+    int encounterRate;
+};
+
+Grass forestGrass = {
+    "Forest",
+    {{"Pidgey", PokemonType:: NORMAL, 40}, {"Caterpie", PokemonType:: BUG, 35}},
+    70
+};
