96475706a3efc2230ee8d8373f0710b046381f35
Parva <parvadesai404@gmail.com>
2024-10-04 20:52:56 +0530

    [Update] object creation using pointers

--- Main.cpp
+++ Main.cpp
@@ -5,15 +5,19 @@
 
 int main()
 {
-    N_Character::ProfessorOak professor("Professor Oak");
-    N_Character::N_Player::Player player;
+    N_Character::ProfessorOak* professor = new N_Character::ProfessorOak("Professor Oak");
+    N_Character::N_Player::Player* player = new N_Character::N_Player::Player();
 
-    professor.greetPlayer(player); 
-    professor.offerPokemonChoices(player);
-    professor.explainMainQuest(player);
+    professor->greetPlayer(*player); 
+    professor->offerPokemonChoices(*player);
+    professor->explainMainQuest(*player);
 
-    N_Main::Game game;
-    game.gameLoop(player);
+    N_Main::Game* game = new N_Main::Game();
+    game->gameLoop(*player);
 
+    delete(professor);
+    delete(player);
+    delete(game);
+    
     return 0;
 }
