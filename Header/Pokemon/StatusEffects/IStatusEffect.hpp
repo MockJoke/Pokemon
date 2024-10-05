c388977b5e3e7810b9c9e7790337c671401d6a02
Parva <parvadesai404@gmail.com>
2024-10-05 20:49:12 +0530

    [Add] paralyzed status effect

---
+++ Header/Pokemon/StatusEffects/IStatusEffect.hpp
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <string>
+
+namespace N_Pokemon
+{
+    class Pokemon;
+    
+    namespace N_StatusEffects
+    {
+        class IStatusEffect
+        {
+        public:
+            // Apply the effect (e.g., poison, burn)
+            virtual void applyEffect(Pokemon* target) = 0;
+
+            // Get the name of the effect
+            virtual std::string getEffectName() = 0;
+
+            // Apply the changes due to effect after the end of each turn
+            // Returns true if the Pokémon can move, else false
+            virtual bool turnEndEffect(Pokemon* target) = 0;
+
+            // Remove the effect
+            virtual void clearEffect(Pokemon* target) = 0;
+
+            virtual ~IStatusEffect() = default;
+        };
+    }
+}
